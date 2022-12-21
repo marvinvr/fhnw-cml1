603d08b60767e76ed4e94cb73c4bcfbcf34c8bee
Marvin von Rappard <marvin@von-rappard.ch>
2022-12-21 18:18:06 +0100

    adds params

--- helpers/training.py
+++ helpers/training.py
@@ -84,16 +84,16 @@ def train_gradient_boosting_v1(X_train: pd.DataFrame,
                             X_test: pd.DataFrame,
                             y_train: pd.Series,
                             y_test: pd.Series) -> dict:
-    '''parameters = {
-        'loss': ['absolute_error'],
-        'max_depth': [17, 19, 21, 23, 25],  #[26],
-        'min_samples_split': [1, 2],
-        'min_samples_leaf': [12, 13, 15, 17],
-        'max_features': ['sqrt'],
-        'n_estimators': [120, 130, 140],
-        'random_state': [42]
-    }'''
     parameters = {
+        'loss': ['absolute_error'],
+        'max_depth': [13, 17, 21, 25, 29],  #[26],
+        'min_samples_split': [1, 2, 3, 4],
+        'min_samples_leaf': [9, 12, 15, 18],
+        'max_features': ['sqrt', 'auto'],
+        'n_estimators': [110, 130, 150],
+        'random_state': [42]
+    }
+    '''parameters = {
         'loss': ['absolute_error'],
         'max_depth': [21],  # [26],
         'min_samples_split': [1],
@@ -101,7 +101,7 @@ def train_gradient_boosting_v1(X_train: pd.DataFrame,
         'max_features': ['sqrt'],
         'n_estimators': [130],
         'random_state': [42]
-    }
+    }'''
 
 
     # NOT WORKING, TRANSFORMED KAGGLE
